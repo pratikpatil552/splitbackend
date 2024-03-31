0ed0dc95baca340a7abb736a6faf41b847f3a0ae
Pratik Patil <pratikpatilmudalp3@gmail.com>
2024-03-31 14:28:54 +0530

    started with frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 
@@ -14,7 +15,12 @@ mongoose
     .then((e)=>{console.log("mongo db connected")});
 
 // middlewares used here
-app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors({
+    credentials : true,
+    origin : "http://localhost:3000",
+}));
+
 //app.use(cookieParser());
 
 // it is the test route
